635803f5c2b6644eb4e625d4934b76a571db2e7e
Fizzify <21076@brightwoods.edu.ph>
2022-04-07 13:22:46 +0800

    Added source.

---
+++ src/components/Facts.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let fact;
+</script>
+
+<body>
+  <h3>MaxGen(A dog facts generator)</h3>
+  <h1>{fact.fact}</h1>
+  <a href="https://github.com/Fizzify/maxgen" target="__blank">Github</a>
+  <p>Made by <a href="https://github.com/Fizzify" target="__blank">Fizzify</a></p>
+</body>
+
+<style>
+
+  a {
+    color: blue;
+  }
+
+  h1, p {
+    padding: 1rem;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,41 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1');
+		const data = await res.json();
+		if (res.ok) {
+			return {
+				props: { fact: data[0] }
+			};
+		}
+	}
+</script>
+
+<script>
+	import Facts from '../components/Facts.svelte';
+	export let fact;
+</script>
+
+<body>
+	<section>
+		<Facts {fact} />
+	</section>
+</body>
+
+<style>
+	*,
+	*::after,
+	*::before {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body {
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		font-family: Arial;
+	}
+</style>
